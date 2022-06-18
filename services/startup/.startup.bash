207e172dfe7b803b7ccb020f6c4a73a29dd75c99
ExposedCat <artem13.prokop@gmail.com>
2022-06-18 17:32:56 +0200

    ⭐️ Add startup service + script

---
+++ services/startup/.startup.bash
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+# Allow to modify brightness of ScreenPad
+sudo chmod a+w '/sys/class/leds/asus::screenpad/brightness'
+# Set  maximal brightness of ScreenPad
+screenpad 9
+# Limit batter charge to 80%
+echo 81 | sudo tee -a /sys/class/power_supply/BAT0/charge_control_end_threshold
