d70bee494ebb04a6160a8914c1981c948f329bf8
Artem Prokop <artem13.prokop@gmail.com>
2024-08-17 13:48:55 +0200

    chore: fix typo

--- services/startup/.startup.bash
+++ services/startup/.startup.bash
@@ -4,5 +4,5 @@
 sudo chmod a+w '/sys/class/leds/asus::screenpad/brightness'
 # Set  maximal brightness of ScreenPad
 screenpad 9
-# Limit batter charge to 80%
+# Limit battery charge to 80%
 echo 81 | sudo tee -a /sys/class/power_supply/BAT0/charge_control_end_threshold
